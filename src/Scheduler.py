2e002fd9cf02fc5be922fd2e63e0db9556e066de
Ben Collins <sstben@gmail.com>
2021-03-24 21:16:41 -0700

    Updated scheduler for weekly schedules

--- src/Scheduler.py
+++ src/Scheduler.py
@@ -2,12 +2,24 @@ import schedule
 import time
 import importlib
 import datetime
+import enum
 
 
 sim = importlib.import_module('GetDailyShortInterest').ShortInterestManager
 nso = importlib.import_module('NasdaqShareOrdersScraper').NasdaqShareOrdersManager
 UES = importlib.import_module('UpcomingEarningsScanner').main
 Utils = importlib.import_module('utilities').Utils
+
+
+# For representing days of the week
+class Days(enum.Enum):
+    Sun = 1
+    Mon = 2
+    Tue = 3
+    Wed = 4
+    Thu = 5
+    Fri = 6
+    Sat = 7
 
 
 class ScheduleManager:
@@ -17,7 +29,9 @@ class ScheduleManager:
 
         sim_obj = sim()
         res = sim_obj.get_latest_short_interest_data()
-        Utils.upload_file_to_gdrive(res, 'Daily Short Data')
+        for r in res:
+            sub_dir = '/'.join(r.split('/')[2:-1])  # Just get subdirectory path
+            Utils.upload_file_to_gdrive(r, 'Daily Short Data')
 
     @staticmethod
     def call_nasdaq_share_orders():
@@ -27,6 +41,42 @@ class ScheduleManager:
     @staticmethod
     def call_upcoming_earnings_scanner():
         UES()
+
+    @staticmethod
+    def loop_schedule_task_weekly(task, day, dtime_lower='00:00', dtime_upper='23:59'):
+
+        if day == Days.Sun:
+            schedule.every().sunday.at(dtime_lower).do(task)
+        elif day == Days.Mon:
+            schedule.every().monday.at(dtime_lower).do(task)
+        elif day == Days.Tue:
+            schedule.every().tuesday.at(dtime_lower).do(task)
+        elif day == Days.Wed:
+            schedule.every().wednesday.at(dtime_lower).do(task)
+        elif day == Days.Thu:
+            schedule.every().thursday.at(dtime_lower).do(task)
+        elif day == Days.Fri:
+            schedule.every().friday.at(dtime_lower).do(task)
+        elif day == Days.Sat:
+            schedule.every().saturday.at(dtime_lower).do(task)
+
+        d_lower = datetime.datetime.strptime(dtime_lower, '%H:%M').time()
+        d_upper = datetime.datetime.strptime(dtime_upper, '%H:%M').time()
+
+        next_run_date = datetime.datetime.today()
+
+        while True:
+
+            schedule.run_pending()
+            time.sleep(1)  # Only check every 30 minutes
+
+            n = datetime.datetime.now()
+            curr_time = n.time()
+
+            if d_lower < curr_time < d_upper and n.date() == next_run_date.date():
+                schedule.run_all()
+
+            next_run_date = schedule.next_run()
 
     @staticmethod
     def loop_schedule_task_days(task, num_days=1, dtime_lower='00:00', dtime_upper='23:59'):
