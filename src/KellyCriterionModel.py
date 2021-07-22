79a0d50535fec857a666217203cca8291458d7f7
Fedya <fedsemenov52@gmail.com>
2021-07-21 19:00:22 -0700

    Worked on KellyCriterionModel.py, Created TweetDatabaseManager.py.

--- src/KellyCriterionModel.py
+++ src/KellyCriterionModel.py
@@ -3,10 +3,41 @@ import pandas as pd
 
 
 class KellyModel:
-    def __init__(self, beg_capital=10000, edge=0.5, glr=1.15, fractional_kelly=0.25, l_ceiling=0.2, w_ceiling=0.5,
-                 num_runs=100, tax_rate=0.1, snp=0.1478, expected_return=0.0008):
-        self.beg_capital, self.edge, self.glr, self.fractional_kelly, self.l_ceiling, self.w_ceiling, self.num_runs =\
-            beg_capital, edge, glr, fractional_kelly, l_ceiling, w_ceiling, num_runs
+    """
+    Used to simulate trading with the kelly model, either single simulation of n trades or multiple simulations of n
+    trades.
+    """
+    def __init__(self, beg_capital=10000, edge=0.5, edge_range=0, glr=1.15, fractional_kelly=0.25, l_ceiling=0.2,
+                 w_ceiling=0.5, num_runs=100, tax_rate=0.1, snp=0.1478, expected_return=0.0008):
+        """
+        :param beg_capital: Starting capital (USD)
+        :type beg_capital: float
+        :param edge: Average chance of winning a trade in (0, 1)
+        :type edge: float
+        :param  edge_range: The range for the edge for any specific trade:
+                            (Current Edge = Edge + random.uniform(-range, +range)
+        :type edge_range: float
+        :param glr: Gain loss ratio in (0, 1)
+        :type glr: float
+        :param fractional_kelly: The fraction of the Kelly model to use in (0, 1)
+        :type fractional_kelly: float
+        :param l_ceiling: Loss ceiling percentage wise for any given trade in (0, 1)
+        :type l_ceiling: float
+        :param w_ceiling: Winn ceiling percentage wise for any give trade in (0, 1)
+        :type w_ceiling: float
+        :param num_runs: Number of trades to do perform in a single simulation
+        :type num_runs: int
+        :param tax_rate: The tax rate (used to calculate alpha) percentage wise in (0, 1)
+        :type tax_rate: float
+        :param snp: SNP 500 returns (used to calculate alpha) percentage wise in (0, 1)
+        :type snp: float
+        :param expected_return: Expected return per trade (used to calculate Sortino Ratio) percentage wise in (0, 1)
+        :type expected_return: float
+        """
+
+        self.beg_capital, self.edge, self.edge_range, self.glr, self.fractional_kelly, self.l_ceiling, self.w_ceiling, \
+        self.num_runs = \
+            beg_capital, edge, edge_range, glr, fractional_kelly, l_ceiling, w_ceiling, num_runs
 
         self.tax_rate = tax_rate
         self.snp = snp
@@ -43,14 +74,23 @@ class KellyModel:
 
         self.ending_capital = []
 
-        self.keys = ['Ending Capital', 'Wins vs Losses', 'Largest Winning Streak', 'Largest Losing Streak',
+        self.keys = ['Ending Capital List', 'Final Ending Capital', 'Wins vs Losses', 'Largest Winning Streak',
+                     'Largest Losing Streak',
                      'Gross Return', 'Tax Rate', 'Net Return', 'YTD S&P 500 Return', 'Alpha', 'W', 'L',
                      'Total Trades', 'Win Rate', 'Multiple on Invested Capital', 'Largest Gain', 'Largest Loss',
                      'Average Gain', 'Average Loss', 'Expected Gain/(Loss) Per Trade', 'Actual Gain/Loss Ratio',
                      'Downside Risk', 'Average Excess Return', 'Sortino Ratio']
 
     def single_run(self):
-        bet = (self.edge - ((1-self.edge) / self.glr)) * self.fractional_kelly
+        """
+        Simulates a single trade and records the results
+        """
+
+        # Edge calculation for this specific trade using edge range
+        cur_edge = self.edge + random.uniform(-1*self.edge_range, self.edge_range)
+
+        # Kelly model bet size
+        bet = (cur_edge - ((1 - cur_edge) / self.glr)) * self.fractional_kelly
 
         trade_return = 0
         self.num_plays += 1
@@ -59,7 +99,8 @@ class KellyModel:
 
             to_trade = bet * self.curr_capital
 
-            if random.random() <= self.edge:
+            # Win
+            if random.random() <= cur_edge:
                 self.num_wins += 1
                 if self.curr_streak < 0:
                     self.curr_streak = 1
@@ -81,6 +122,7 @@ class KellyModel:
                 if trade_return > self.largest_gain:
                     self.largest_gain = trade_return
 
+            # Loss
             else:
                 self.num_losses += 1
                 if self.curr_streak > 0:
@@ -100,17 +142,23 @@ class KellyModel:
                     self.square_negative_excess_return += ((-1 * diff_in_returns) ** 2)
 
                 self.average_loss -= trade_return
-                if (-1*trade_return) > self.largest_loss:
-                    self.largest_loss = (-1*trade_return)
+                if (-1 * trade_return) > self.largest_loss:
+                    self.largest_loss = (-1 * trade_return)
 
             self.curr_capital += trade_return
             self.ending_capital.append(self.curr_capital)
 
     def all_runs(self):
+        """
+        Simulates a trade (num_runs) times with each trade compounding on the previous
+        """
         for i in range(self.num_runs):
             self.single_run()
 
     def calc_ending_scores(self):
+        """
+        Calculates ending variables after all the trades are completed for a single simulation
+        """
         self.win_v_loss = self.num_wins - self.num_losses
         self.gross_return = (self.curr_capital - self.beg_capital) / self.beg_capital
         self.net_return = self.gross_return * (1 - self.tax_rate)
@@ -129,7 +177,11 @@ class KellyModel:
         self.sortino = self.average_excess_return / self.downside_risk
 
     def to_dict(self):
-        values = [self.ending_capital, self.win_v_loss, self.largest_win_streak, self.largest_loss_streak,
+        """
+        Exports values of a single simulation to a dictionary
+        """
+        values = [self.ending_capital, self.curr_capital, self.win_v_loss, self.largest_win_streak,
+                  self.largest_loss_streak,
                   self.gross_return, self.tax_rate, self.net_return, self.snp, self.alpha, self.num_wins,
                   self.num_losses, self.num_bets, self.win_rate, self.multiple_on_invested_capital, self.largest_gain,
                   self.largest_loss, self.average_gain, self.average_loss, self.expected_gl, self.actual_gl,
@@ -137,17 +189,40 @@ class KellyModel:
 
         return dict(zip(self.keys, values))
 
-    def wrapper(self):
+    def single_sim(self):
+        """
+        Wrapper for running a single simulation
+
+        :return: Pandas dataframe with results
+        :rtype: pd.DataFrame
+        """
         self.all_runs()
         self.calc_ending_scores()
         return self.to_dict()
 
-    def multi_runs(self, num):
+    def multi_sim(self, num):
+        """
+        Wrapper for running multiple simulations (ie running multiple sets of 100 trades to see what outcomes are
+        possible)
+
+        :param num: Number of simulation to run (ie how many times to simulate 100 trades in a row)\
+        :type num: int
+
+        :return: Pandas dataframe with results
+        :rtype: pd.DataFrame
+        """
         df = pd.DataFrame()
 
         for i in range(num):
             self.__init__()
-            df = df.append(self.wrapper(), ignore_index=True)
+            df = df.append(self.single_sim(), ignore_index=True)
             df = df[self.keys]
 
         return df
+
+    @staticmethod
+    def save_df(df: pd.DataFrame, name: str):
+        """
+        Save a dataframe to a csv which can then be opened in Excel for analysis
+        """
+        df.to_csv(name, index=False)
--- src/TweetDatabaseManager.py
+++ src/TweetDatabaseManager.py
@@ -6,6 +6,7 @@ import TweetCollector
 import utilities
 import SqliteManager
 import os
+import time
 
 Utils = utilities.Utils()
 Sqm = SqliteManager.SqliteManager(path='../data/TweetData/TweetDataBase.db')
@@ -13,7 +14,8 @@ Sqm = SqliteManager.SqliteManager(path='../data/TweetData/TweetDataBase.db')
 
 class TweetManager:
     def __init__(self):
-        self.keys = ['Tweet id', 'User id', 'Screen name', 'Label', 'json', 'user.majority_lang', 'botscore',
+        self.keys = ['Tweet id', 'User id', 'Screen name', 'Label', 'Search term', 'json', 'user.majority_lang',
+                     'botscore',
                      'cap.english', 'cap.universal', 'raw_scores.english.astroturf', 'raw_scores.english.fake_follower',
                      'raw_scores.english.financial', 'raw_scores.english.other', 'raw_scores.english.overall',
                      'raw_scores.english.self_declared', 'raw_scores.english.spammer', 'raw_scores.universal.astroturf',
@@ -21,6 +23,7 @@ class TweetManager:
                      'raw_scores.universal.other', 'raw_scores.universal.overall', 'raw_scores.universal.self_declared',
                      'raw_scores.universal.spammer']
         self.BR = br.BotometerRequests()
+        self.path = '../data/TweetData/Tweets'
 
     def modify(self, to_execute: list):
         pass
@@ -28,18 +31,32 @@ class TweetManager:
     def req_tweets(self, keyword: str, num: int):
         tweets = TweetCollector.collect_tweets(phrase=keyword, history_count=num)
         bot_labels = self.BR.wrapper(from_dataframe=tweets, lite_tweet_request=True, bot_user_request=True,
-                                     wanted_bot_keys=([self.keys[5]] + self.keys[7:]))
+                                     wanted_bot_keys=([self.keys[6]] + self.keys[8:]))
         full_df = Utils.basic_merge(tweets, bot_labels)
         full_df = Utils.order_dataframe_columns(full_df, self.keys, cut=True)
 
         return full_df
 
+    def save_tweets(self, keyword: str, num: int):
+        df = self.req_tweets(keyword, num)
+        df.to_csv(self.path + '/' + str(time.time()) + '.csv', index=False)
+        return df
+
     def vertical_merge(self, directory_path):
         files = os.listdir(path=directory_path)
+        full_df = pd.DataFrame(columns=self.keys)
+        for f in files:
+            df = pd.read_csv(directory_path+'/'+f)
+            full_df = pd.concat([full_df, df])
 
+        return full_df
 
+    def merge_and_cut(self, directory_path):
+        df = self.vertical_merge(directory_path)
+        for f in os.listdir(directory_path):
+            os.remove(os.path.join(directory_path, f))
 
-
+        df.to_csv(os.path.join(directory_path, 'Merged.csv'), index=False)
 
 
 TM = TweetManager()
