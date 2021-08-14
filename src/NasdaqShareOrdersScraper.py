143a12aa28021847f9cfba20814d0ba27d862bae
Fedya <fedsemenov52@gmail.com>
2021-08-14 02:24:09 -0700

    Added TweetEDA.py with comments.

--- src/NasdaqShareOrdersScraper.py
+++ src/NasdaqShareOrdersScraper.py
@@ -6,14 +6,14 @@ import datetime
 import pandas as pd
 from os import path
 
-
 Utils = importlib.import_module('utilities').Utils
 
 
 class NasdaqShareOrdersManager:
+    def __init__(self):
+        self.session = requests.Session()
 
-    @staticmethod
-    def get_nasdaq_trade_order(ticker):
+    def get_nasdaq_trade_order(self, ticker):
 
         print('Getting {} data'.format(ticker))
 
@@ -39,27 +39,16 @@ class NasdaqShareOrdersManager:
             data_cmd = 'https://api.nasdaq.com' + url_extension
             header = {'authority': 'api.nasdaq.com',
                       'path': url_extension,
-                      'cookie': 'recentlyViewedList=AMD|Stocks,AAPL|Stocks,NDAQ|Stocks,CRM|Stocks,AMC|Stocks; visid_incap_'
-                                '2170999=AUmeTSAJR9iHbuH7NDYK2V2BG2AAAAAAQUIPAAAAAAARn/KCFFum59zF2XJESLJt; incap_ses_1163_'
-                                '2170999=FDajM6YP1WZckMnOkM4jEF6BG2AAAAAAg286bAKJxp8xhHGwzkcE9g==; bm_mi=8261F0F97854EFC3E'
-                                'A27B7BD8952C115~Iocy2Bqx/0lL/0wb215WggylM9SqS5doA5U1arCMEvcQ0nhvlydEIqkH9hzsC26ds4mX5ymSH'
-                                '2JibJsv8ghqax3d6r6zXcy5yGZ2XSYtv+BTLAcBOAENKbSXCK6kX7EshI+HDZBiZMCg5nY4x1hWDRBpB3Zp0mNFEm'
-                                'V7HFfp30IliewQDjadY6caN96VLH31ntfAdMeTECK8o2wbfUocgMQbvk+Geb8JEPoo6cTUOnQNO3g1FsoM2EPOsPO'
-                                '4wOL8; bm_sv=86C8803AA28B555F82C5561180C57C4D~enV9al4MrEyTm6UC5NTcRoZ8WlrETeGJlo4CJ2n1aNv'
-                                'S1oEQnKc6xqrgw9mrEi94iQJOKmgjVjo1ztV6P6Hxnb7EKU/zNfYsTirjZ3dMGq/6NQ81whPAqnphteVvDVErFY4O'
-                                '4e6lSeTxs9gGRwP8lQFbdIR6/dPgIA7qdyH7WpY=; ak_bmsc=A6FA0CB8CA5A2FF9BB47F7EB78BA661FB81B8CB'
-                                '58A350000787C1B6054EADD42~pla//dtKpllOD61TscYyNh2S2Cu92iiEfC86YpNYoAnSOI93/miqXrVUiyHOhEP'
-                                'uwy44M4NNaoZBM7NXlwPM49U+7iNP/+1bj1PkrbsdRqBLyqeG10LZjICYIEknCBrDqvC/0sha9Xa3D/B30ZfXai0m'
-                                'PsK+ZKTxDhPzZinwIGHNn+bCpQaVo1DbcRNswl/jBQp/GQSFghcfJ0EkIIBgV0IUwj43Zo9MrmgxUgLv5W8SqJItI'
-                                'C1aw95bpK37TkyWL1BL3gp8ko4GvnIbxzCvsgPVZVm7LNhWu+cTmXk8yG6uPhX7QUvMmu0hnb6+7UEwJR; RT="z='
-                                '1&dm=nasdaq.com&si=0lfb4hze973b&ss=kkqdph10&sl=c&tt=36q&obo=7&rl=1"; NSC_W.OEBR.DPN.7070='
-                                'ffffffffc3a0f73345525d5f4f58455e445a4a422dae'}
+                      'cookies': '80566D57C1A0C8E7D449F00A684ACBB9~000000000000000000000000000000~YAAQLvEkF0s53xl7AQAAJA82GgxKO5HGYp6XJfXTVJIgYMRUlvzjBGUi2Uku4UmtkLzlza4Zd4SXgQATwOoE46Iu+wEVF2OZ/vOzTKVefNPv+QJuRLe6/xIS7ViloGmUxaudq19SgPzJk2/Gp0RD6nSuW8enJ/tneLN/ewRtda2nk/MAkfXgZ9iZpkh+ZUUmNvdeCwAesZpLC5p0oMOCrDKnKbTK4t59VCXWeejR5AwjfHFGHdr7mzwgCTo0rCLzyhi4hsWU5v2SO4F5cO489IzIDMQu/NEeRpiR1IwpBqLs0gbq5Kn4+dqG5HwC6+ZTY5ElngiGSm7xAew6XeTyemKNh1lm8UxNXZcXbpQtbca3/t1uk1SqzmQ6+tb/w+FRR0/7UUaoYG9D'}
 
             data = []
             jdata = {'data': None}
             while True:
                 try:
+                    print('Getting')
                     data = requests.get(data_cmd, headers=header)
+                    #data = self.session.get(data_cmd, headers=header)
+                    print('Got')
                     jdata = json.loads(data.text)
                     break
                 except:
