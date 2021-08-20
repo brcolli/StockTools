55a843a489995fa13eee5af7957197e646e74b3a
Ben Collins <sstben@gmail.com>
2021-08-19 19:38:31 -0700

    quiver quant function add

--- src/DataAugmentationManager.py
+++ src/DataAugmentationManager.py
@@ -1,7 +1,63 @@
+from numpy import mean
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.tree import DecisionTreeClassifier
+from imblearn.pipeline import Pipeline
+from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import ADASYN
+from imblearn.under_sampling import RandomUnderSampler
+from collections import Counter
+from imblearn.over_sampling import BorderlineSMOTE
+
+
 class DataAugmentationManager:
 
     def __init__(self):
         return
+
+    def smote_for_classification(self, x, y):
+
+        # values to evaluate
+        k_values = [1, 2, 3, 4, 5, 6, 7]
+        for k in k_values:
+
+            # define pipeline
+            model = DecisionTreeClassifier()
+            over = SMOTE(sampling_strategy=0.1, k_neighbors=k)
+            under = RandomUnderSampler(sampling_strategy=0.5)
+            steps = [('over', over), ('under', under), ('model', model)]
+            pipeline = Pipeline(steps=steps)
+
+            # evaluate pipeline
+            cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+            scores = cross_val_score(pipeline, x, y, scoring='roc_auc', cv=cv, n_jobs=-1)
+            return mean(scores)
+
+    def borderline_smote(self, x, y):
+
+        # summarize class distribution
+        counter = Counter(y)
+        print(counter)
+
+        # transform the dataset
+        oversample = BorderlineSMOTE()
+        x, y = oversample.fit_resample(x, y)
+
+        # summarize the new class distribution
+        return Counter(y)
+
+    def adaptive_synthetic_sampling(self, x, y):
+
+        # summarize class distribution
+        counter = Counter(y)
+        print(counter)
+
+        # transform the dataset
+        oversample = ADASYN()
+        x, y = oversample.fit_resample(x, y)
+
+        # summarize the new class distribution
+        return Counter(y)
 
 
 def main():
---
+++ src/quiver.py
@@ -0,0 +1,20 @@
+import quiverquant
+import importlib
+
+
+Utils = importlib.import_module('utilities').Utils
+
+ticker = ''
+politician = ''
+
+quiver = quiverquant.quiver('bf4263dfcfd881ece6d8ab45852fe9c27b4b5508')
+
+if ticker == '' and politician == '':
+    data = quiver.congress_trading()
+elif politician == '':
+    data = quiver.congress_trading(ticker)
+else:
+    data = quiver.congress_trading(politician, politician=True)
+
+print(data)
+Utils.write_dataframe_to_csv(data, '../data/')
