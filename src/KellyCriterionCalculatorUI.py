b0f04310f994b0b1e1569697463d63a919859a39
Fedya <fedsemenov52@gmail.com>
2021-12-03 14:18:50 -0800

    KellyCriterionCalculatorUI.py is now functional.

--- src/KellyCriterionCalculatorUI.py
+++ src/KellyCriterionCalculatorUI.py
@@ -1,34 +1,91 @@
+import re
 import sys
-import importlib
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit, QLabel, QMainWindow
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
-import pandas as pd
-import KellyCriterion
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox
 
-KCUI = 0
+from KellyDesign import Ui_MainWindow
 
-# window = tk.Tk()
-#
-# frame_p = tk.Frame()
-# label_p = tk.Label(master=frame_p, text="Enter the Probability of winning (0.00-1.00)")
-#
-#
-# probability = tk.Entry(text="Probability of winning (0.00-1.00)")
-# odds = tk.Entry(text="Probability of winning (0.00-1.00)")
-# bankroll = tk.Entry(text="Probability of winning (0.00-1.00)")
-# fractional_kelly = tk.Entry(text="Probability of winning (0.00-1.00)")
-#
-# greeting.pack()
-# probability.pack()
-# odds.pack()
-# bankroll.pack()
-# fractional_kelly.pack()
-#
-#
-# window.mainloop()
 
-class QTManager(QMainWindow):
-    pass
-def main():
-    pass
+class Window(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.connectSignalsSlots()
+
+        self.portValue.valueChanged.connect(self.p_value)
+        self.probWinning.valueChanged.connect(self.p_winning)
+        self.odds.valueChanged.connect(self.o_winning)
+        self.fracKelly.valueChanged.connect(self.f_kelly)
+
+        self.clearButton.clicked.connect(self.clear_values)
+
+        self.PFValue = 0
+        self.PoW = 0
+        self.Odds = 0.1
+        self.FKelly = 0
+        self.AmountBet = 0
+        self.PercentageBet = 0
+        self.set_initial_values()
+
+    def set_initial_values(self):
+        self.PFValue = self.portValue.value()
+        self.PoW = self.probWinning.value()
+        self.Odds = self.odds.value()
+        self.FKelly = self.fracKelly.value()
+        self.update_outcomes()
+
+    def clear_values(self):
+        self.PFValue = 0
+        self.PoW = 0
+        self.Odds = 0.1
+        self.FKelly = 0
+        self.AmountBet = 0
+        self.PercentageBet = 0
+
+        self.portValue.clear()
+        self.probWinning.clear()
+        self.odds.clear()
+        self.fracKelly.clear()
+        self.percentOut.clear()
+        self.amountOut.clear()
+
+    def p_value(self, num):
+        self.PFValue = num
+        self.update_outcomes()
+
+    def p_winning(self, num):
+        self.PoW = num
+        self.update_outcomes()
+
+    def o_winning(self, num):
+        self.Odds = num
+        self.update_outcomes()
+
+    def f_kelly(self, num):
+        self.FKelly = num
+        self.update_outcomes()
+
+    def update_outcomes(self):
+        win = self.PoW / 100
+        loss = 1 - (self.PoW/100)
+
+        self.PercentageBet = (win - (loss / self.Odds)) * (self.FKelly / 100)
+        self.AmountBet = self.PercentageBet * self.PFValue
+
+        self.percentOut.setValue(self.PercentageBet * 100)
+        self.amountOut.setValue(self.AmountBet)
+
+
+    def callto(self):
+        print('K')
+
+    def connectSignalsSlots(self):
+        self.amountOut.update()
+        # self.action_Exit.triggered.connect(self.close)
+        # self.action_Find_Replace.triggered.connect(self.findAndReplace)
+        # self.action_About.triggered.connect(self.about)
+
+
+app = QApplication(sys.argv)
+win = Window()
+win.show()
+sys.exit(app.exec())
